e7a6fdba4aef3e16a1f6df59b5420e98439f56f4
Yusra <yusrashah115@gmail.com>
2026-05-19 23:04:45 +0500

    fix ALLOWED_HOSTS for railway

--- tutoring_platform/settings.py
+++ tutoring_platform/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".railway.app"
+]
 
 
 # Application definition
