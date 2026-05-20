689e0713ef44b21eaec84749ead088f911494191
Yusra <yusrashah115@gmail.com>
2026-05-20 06:30:59 +0500

    fix wsgi settings module path

--- tutoring_platform/asgi.py
+++ tutoring_platform/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutoring_platform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peer_tutoring.settings')
 
 application = get_asgi_application()
--- tutoring_platform/wsgi.py
+++ tutoring_platform/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tutoring_platform.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'peer_tutoring.settings')
 
 application = get_wsgi_application()
